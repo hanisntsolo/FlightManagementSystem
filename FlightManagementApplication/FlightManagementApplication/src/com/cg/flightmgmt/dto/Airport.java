7e70137631f847aa500c19164d107dd19c0d99d8
Dhirendra Pratap Singh <36436712+hanisntsolo@users.noreply.github.com>
2020-10-19 22:45:22 +0530

    //Airport edited by Dhirendra

--- FlightManagementApplication/FlightManagementApplication/src/com/cg/flightmgmt/dto/Airport.java
+++ FlightManagementApplication/FlightManagementApplication/src/com/cg/flightmgmt/dto/Airport.java
@@ -1,8 +1,70 @@
 package com.cg.flightmgmt.dto;
 
+import java.util.Objects;
+
 public class Airport {
-	private int airportid;
+	private int airportId;
 	private String airportName;
 	private String airportLocation;
-	
+
+	public Airport() {
+	}
+
+	public Airport(int airportId, String airportName, String airportLocation) {
+		this.airportId = airportId;
+		this.airportName = airportName;
+		this.airportLocation = airportLocation;
+	}
+
+	public int getAirportId() {
+		return airportId;
+	}
+
+	public void setAirportId(int airportId) {
+		this.airportId = airportId;
+	}
+
+	public String getAirportName() {
+		return airportName;
+	}
+
+	public void setAirportName(String airportName) {
+		this.airportName = airportName;
+	}
+
+	public String getAirportLocation() {
+		return airportLocation;
+	}
+
+	public void setAirportLocation(String airportLocation) {
+		this.airportLocation = airportLocation;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) {
+			return true;
+		}
+		if (o == null || getClass() != o.getClass()) {
+			return false;
+		}
+		Airport airport = (Airport) o;
+		return airportId == airport.airportId &&
+				airportName.equals(airport.airportName) &&
+				airportLocation.equals(airport.airportLocation);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(airportId, airportName, airportLocation);
+	}
+
+	@Override
+	public String toString() {
+		return "Airport{" +
+				"airportId=" + airportId +
+				", airportName='" + airportName + '\'' +
+				", airportLocation='" + airportLocation + '\'' +
+				'}';
+	}
 }
